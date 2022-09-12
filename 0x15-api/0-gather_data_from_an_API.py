6206039f86ad3657121bfebdac6d5a484b38c856
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-12 19:46:52 +0100

    initial

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""returns information about his/her TOD_O list progress."""
+from requests import get
+from sys import argv
+
+if __name__ == "__main__":
+    id = int(argv[1])
+    url = ('https://jsonplaceholder.typicode.com')
+    user = get("{}/users/{}".format(url, id)).json()
+    target = get("{}/users/{}/todos".format(url, id)).json()
+    u_name = user.get('name')
+    done = 0
+    for i in target:
+        if i['completed']:
+            done += 1
+    print("Employee {} is done with tasks({}/{}):".format(u_name, done, len(target)))
+    for done in target:
+        if done.get('completed'):
+            print("\t {}".format(done.get('title')))
