c7ab8c945f37824d9a7d331120b673d435137e6a
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-12 20:59:01 +0100

    fixed it

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -8,14 +8,14 @@ if __name__ == "__main__":
     id = argv[1]
     url = "https://jsonplaceholder.typicode.com"
     user = get("{}/users/{}".format(url, id)).json()
-    target = get("{}/users/{}/todos".format(url, id)).json()
+    target = get('{}/todos?userId={}'.format(url, id)).json()
     u_name = user.get('username')
     fmt = {}
     fmt[id] = []
     for i in target:
-        append = {'task': i['title'], 'completed': i['completed'],
-                  'username': u_name}
-        fmt[id].append(append)
-    file_format = '{}.csv'.format(id)
+        appends = {'task': i['title'], 'completed': i['completed'],
+                   'username': u_name}
+        fmt[id].append(appends)
+    file_format = '{}.json'.format(id)
     with open(file_format, 'w') as result:
         dump(fmt, result)
