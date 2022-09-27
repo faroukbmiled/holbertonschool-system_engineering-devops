30af6968de128280f21726af880b02bfb4420982
faroukbmiled <fcfaroukfc@gmail.com>
2022-09-27 22:53:27 +0100

    finished

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -4,17 +4,17 @@ import requests
 
 
 def recurse(subreddit, hot_list=[], after=''):
-    """rec func"""
-    url = 'http://reddit.com/r/{}/hot.json'.format(subreddit)
+    url = 'https://api.reddit.com/r/{}/hot?after={}'.format(subreddit, after)
     headers = headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0;rv:68.0)'}
-    req = requests.get(url, headers=headers, params={'limit': 100})
+    req = requests.get(url, headers=headers,
+                       params={'limit': 100}, allow_redirects=False)
     if req.status_code == 200:
         data = req.json().get('data').get('children')
         chk = req.json().get('data').get('after')
         for elem in data:
             hot_list.append(elem.get('data').get('title'))
-        if chk is None:
-            recurse(subreddit, hot_list, after)
+        if chk:
+            recurse(subreddit, hot_list, chk)
         return hot_list
-    elif req.status_code != 200:
+    elif req.status_code == 404:
         return None
