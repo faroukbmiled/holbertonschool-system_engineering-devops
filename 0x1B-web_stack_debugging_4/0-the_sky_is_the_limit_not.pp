dd948bd92a00855e2f6ad604e275f0e896c0f23b
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-06 00:52:41 +0100

    test

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,7 @@ exec { 'fix':
   command => 'rm -f /etc/default/nginx && sudo service nginx restart',
   path    => '/usr/local/bin/:/bin/'
 }
+exec {'fix':
+  command  => 'sudo service nginx restart',
+  path    => '/usr/local/bin/:/bin/'
+}
