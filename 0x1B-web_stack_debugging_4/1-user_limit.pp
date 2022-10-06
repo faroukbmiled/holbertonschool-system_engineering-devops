c6be94dcfc0ad915040b46e8e329b35e146725e4
faroukbmiled <fcfaroukfc@gmail.com>
2022-10-06 01:13:11 +0100

    text

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,4 +1,9 @@
 # rm usr limit
 exec { 'fix':
-  command => "/bin/sed -i 's/holberton//g' /etc/security/limits.conf"
+  command => "/bin/sed -i 's/holberton/none/g' /etc/security/limits.conf"
+}
+
+exec { 'fix2':
+  command => "/bin/sed -i 's/holberton/none/g' /etc/security/limits.conf",
+  require => Exec['fix']
 }
